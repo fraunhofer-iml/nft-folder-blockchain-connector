03b4bba2e6d4751664ce229b9824297cc8385ec1
Lukas Grieger <lukas.grieger@iml.fraunhofer.de>
2023-04-03 14:48:51 +0200

    modified token endpoints

--- src/modules/token/controller/token.controller.ts
+++ src/modules/token/controller/token.controller.ts
@@ -7,20 +7,75 @@ export class TokenController {
 
     constructor(private readonly tokenConnectorService: TokenConnectorService) {}
 
+    @Post("addTokenToSegment")
+    public addTokenToSegment(tokenId: number, segmentAddress: string){
+        return this.tokenConnectorService.addTokenToSegment(tokenId, segmentAddress);
+    }
+
+    @Post("approve")
+    public approve(to: string, tokenId: number){
+        return this.tokenConnectorService.approve(to, tokenId);
+    }
+
+    @Post("burn")
+    public burn(tokenId: number){
+        return this.tokenConnectorService.burn(tokenId);
+    }
+
+    @Post("removeTokenFromSegment")
+    public removeTokenFromSegment(tokenId: number, segmentAddress: string){
+        return this.tokenConnectorService.removeTokenFromSegment(tokenId, segmentAddress);
+    }
+
+    @Post("renounceOwnership")
+    public renounceOwnership(){
+        return this.tokenConnectorService.renounceOwnership();
+    }
 
     @Post("safeMint")
     public safeMint(@Body() tokenMintDto: TokenMintDto){
         return this.tokenConnectorService.safeMint(tokenMintDto);
     }
 
-    @Get("getTokenURI/:tokenId")
-    public getTokenURI(@Param("tokenId") tokenId: number){
-        return this.tokenConnectorService.getTokenURI(tokenId);
+    @Post("safeTransferFrom")
+    public safeTransferFrom(from: string, to: string, tokenId: number){
+        return this.tokenConnectorService.safeTransferFrom(from, to, tokenId);
+
+    }
+
+    @Post("setApprovalForAll")
+    public setApprovalForAll(operator: string, approved: boolean){
+        return this.tokenConnectorService.setApprovalForAll(operator, approved);
+
+    }
+
+    @Post("transferFrom")
+    public transferFrom(from: string, to: string, tokenId: boolean){
+        return this.tokenConnectorService.transferFrom(from, to, tokenId);
+
+    }
+
+    @Post("transferOwnership")
+    public transferOwnership(newOwner: string){
+        return this.tokenConnectorService.transferOwnership(newOwner);
+
+    }
+
+    @Post("balanceOf")
+    public balanceOf(ownerAddress: string){
+        return this.tokenConnectorService.balanceOf(ownerAddress);
+
     }
 
     @Get("getAdditionalInformation/:tokenAddress")
     public getAdditionalInformation(tokenId: number, @Param("tokenAddress") tokenAddress: string){
         return this.tokenConnectorService.getAdditionalInformation(tokenId);
+
+    }
+
+    @Post("getApproved")
+    public getApproved(tokenId: number){
+        return this.tokenConnectorService.getApproved(tokenId);
 
     }
 
@@ -30,11 +85,73 @@ export class TokenController {
 
     }
 
+    @Post("getAssetUri")
+    public getAssetUri(tokenId: number){
+        return this.tokenConnectorService.getAssetUri(tokenId);
+    }
+
+    @Post("getIndexForTokenAtSegment")
+    public getIndexForTokenAtSegment(tokenId: number, segmentAddress: string){
+        return this.tokenConnectorService.getIndexForTokenAtSegment(tokenId, segmentAddress);
+
+    }
+
     @Get("getMetadataHash/:tokenAddress")
     public getMetadataHash(tokenId: number, @Param("tokenAddress") tokenAddress: string){
         return this.tokenConnectorService.getMetadataHash(tokenId);
 
     }
+
+    @Post("getSegmentCountByToken")
+    public getSegmentCountByToken(tokenId: number){
+        return this.tokenConnectorService.getSegmentCountByToken(tokenId);
+    }
+
+    @Post("getSegmentForTokenAtSegmentIndex")
+    public getSegmentForTokenAtSegmentIndex(tokenId: number, segmentIndex: string){
+        return this.tokenConnectorService.getSegmentForTokenAtSegmentIndex(tokenId, segmentIndex);
+    }
+
+    @Post("isApprovedForAll")
+    public isApprovedForAll(owner: string, operator: string){
+        return this.tokenConnectorService.isApprovedForAll(owner, operator);
+    }
+
+    @Post("isTokenInSegment")
+    public isTokenInSegment(tokenId: number, segmentAddress: string){
+        return this.tokenConnectorService.isTokenInSegment(tokenId, segmentAddress);
+    }
+
+    @Post("name")
+    public name(){
+        return this.tokenConnectorService.name();
+    }
+
+    @Post("owner")
+    public owner(){
+        return this.tokenConnectorService.owner();
+    }
+
+    @Post("ownerOf")
+    public ownerOf(tokenId: number){
+        return this.tokenConnectorService.ownerOf(tokenId);
+    }
+
+    @Post("supportsInterface")
+    public supportsInterface(interfaceId: string){
+        return this.tokenConnectorService.supportsInterface(interfaceId);
+    }
+
+    @Post("symbol")
+    public symbol(){
+        return this.tokenConnectorService.symbol();
+    }
+
+    @Get("getTokenURI/:tokenId")
+    public getTokenURI(@Param("tokenId") tokenId: number){
+        return this.tokenConnectorService.getTokenURI(tokenId);
+    }
+
 
     @Post("setMetadata/:tokenAddress")
     public setMetadata(tokenId: number, tokenUri: string, metadataHash: string, @Param("tokenAddress") tokenAddress: string){
--- src/modules/token/sc-connector/token.connector.service.ts
+++ src/modules/token/sc-connector/token.connector.service.ts
@@ -1,7 +1,6 @@
 import {TokenMintDto} from "../../../dto/TokenMint.dto";
 import {BlockchainConnectorService} from "../../blockchain-connector/blockchain-connector.service";
 import {ApiConfigService} from "../../../settings/ApiConfigService";
-import {containerAbi} from "../../container/sc-connector/container.abi";
 import {Injectable} from "@nestjs/common";
 import {TokenAbi} from "./token.abi";
 
@@ -20,6 +19,29 @@ export class TokenConnectorService {
         );
     }
 
+    public addTokenToSegment(tokenId: number, segmentAddress: string){
+        return this.blockchainConnectorService.sendTransaction(this.contract.methods.addTokenToSegment(tokenId, segmentAddress));
+
+    }
+
+    public approve(to: string, tokenId: number){
+        return this.blockchainConnectorService.sendTransaction(this.contract.methods.approve(to, tokenId));
+
+    }
+
+    public burn(tokenId: number){
+        return this.blockchainConnectorService.sendTransaction(this.contract.methods.burn(tokenId));
+
+    }
+
+    public removeTokenFromSegment(tokenId: number, segmentAddress: string){
+        return this.blockchainConnectorService.sendTransaction(this.contract.methods.removeTokenFromSegment(tokenId, segmentAddress));
+
+    }
+
+    public renounceOwnership(){
+        return this.blockchainConnectorService.sendTransaction(this.contract.methods.renounceOwnership());
+    }
 
     public safeMint(tokenMintDto: TokenMintDto){
 
@@ -41,24 +63,100 @@ export class TokenConnectorService {
             return this.blockchainConnectorService.sendTransaction(transaction);
     }
 
-    public getTokenURI(tokenId: number){
-        console.log(this.contract)
-        return this.blockchainConnectorService.call(this.contract.methods.tokenURI(tokenId));
+    public safeTransferFrom(from: string, to: string, tokenId: number){
+        return this.blockchainConnectorService.sendTransaction(this.contract.methods.safeTransferFrom(from, to, tokenId));
+
+    }
+
+    public setApprovalForAll(operator: string, approved: boolean){
+        return this.blockchainConnectorService.sendTransaction(this.contract.methods.setApprovalForAll(operator, approved));
+
+    }
+
+    public transferFrom(from: string, to: string, tokenId: boolean){
+        return this.blockchainConnectorService.sendTransaction(this.contract.methods.transferFrom(from, to, tokenId));
+
+    }
+
+    public transferOwnership(newOwner: string){
+        return this.blockchainConnectorService.sendTransaction(this.contract.methods.transferOwnership(newOwner));
+
+    }
+
+    public balanceOf(ownerAddress: string){
+        return this.blockchainConnectorService.call(this.contract.methods.balanceOf(ownerAddress));
+
     }
 
     public getAdditionalInformation(tokenId: number){
         return this.blockchainConnectorService.call(this.contract.methods.getAdditionalInformation(tokenId));
     }
 
+    public getApproved(tokenId: number){
+        return this.blockchainConnectorService.call(this.contract.methods.getApproved(tokenId));
+
+    }
     public getAssetHash(tokenId: number){
         return this.blockchainConnectorService.call(this.contract.methods.getAssetHash(tokenId));
+    }
+    public getAssetUri(tokenId: number){
+        return this.blockchainConnectorService.call(this.contract.methods.getAssetUri(tokenId));
+    }
+
+    public getIndexForTokenAtSegment(tokenId: number, segmentAddress: string){
+        return this.blockchainConnectorService.call(this.contract.methods.getIndexForTokenAtSegment(tokenId, segmentAddress));
+
     }
 
     public getMetadataHash(tokenId: number){
         return this.blockchainConnectorService.call(this.contract.methods.getMetadataHash(tokenId));
     }
 
+    public getSegmentCountByToken(tokenId: number){
+        return this.blockchainConnectorService.call(this.contract.methods.getSegmentCountByToken(tokenId));
+    }
+
+    public getSegmentForTokenAtSegmentIndex(tokenId: number, segmentIndex: string){
+        return this.blockchainConnectorService.call(this.contract.methods.getSegmentForTokenAtSegmentIndex(tokenId, segmentIndex));
+    }
+
+    public isApprovedForAll(owner: string, operator: string){
+        return this.blockchainConnectorService.call(this.contract.methods.isApprovedForAll(owner, operator));
+    }
+
+    public isTokenInSegment(tokenId: number, segmentAddress: string){
+        return this.blockchainConnectorService.call(this.contract.methods.isTokenInSegment(tokenId, segmentAddress));
+    }
+
+
+
+    public name(){
+        return this.blockchainConnectorService.call(this.contract.methods.name());
+    }
+
+    public owner(){
+        return this.blockchainConnectorService.call(this.contract.methods.owner());
+    }
+
+    public ownerOf(tokenId: number){
+        return this.blockchainConnectorService.call(this.contract.methods.ownerOf(tokenId));
+    }
+
+    public supportsInterface(interfaceId: string){
+        return this.blockchainConnectorService.call(this.contract.methods.supportsInterface(interfaceId));
+    }
+
+    public symbol(){
+        return this.blockchainConnectorService.call(this.contract.methods.symbol());
+    }
+
+    public getTokenURI(tokenId: number){
+        return this.blockchainConnectorService.call(this.contract.methods.tokenURI(tokenId));
+    }
+
+
     public setMetadata(tokenId: number, tokenUri: string, metadataHash: string){
         return this.blockchainConnectorService.sendTransaction(this.contract.methods.setMetadata(tokenId, tokenUri, metadataHash));
     }
+
 }
